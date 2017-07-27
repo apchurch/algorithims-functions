d70e45f5cea9a8ed3e3d3a666480570046f1e757
Andrew Church <andrewchurch@Andrews-MacBook-Pro.local>
2017-07-27 16:07:00 -0600

    Add existing file

--- algorithims-functions-assignment.js
+++ algorithims-functions-assignment.js
@@ -1,22 +1,15 @@
 
-var findMode = function(arr) {
-	var mode = 0;
-	for (var i = 0; i < arr.length; i++) {
-		for (var j = 0; j < i; j++) {
-			if (arr[j] === arr[i]) {
-				mode = arr[j];
-				return mode
-				}
-			}
-		}
-		
-	}
-console.log(findMode);
-console.log(findMode([2, 4, 4, 4, 6, 6, 1]))
+var mode = function(arr){
+	return arr.sort((a,b) =>
+			arr.filter(v => v===a).length
+			- arr.filter(v => v===b).length
+		).pop()
+}
+console.log(mode([1, 2, 2, 3, 4, 4, 4, 4, 6]))
 
 var leapYear = function(number) {
-	for (var leap = 1904; leap < 2017; leap +=4) {
-			if(number === leap) {
+	for (var leap = 1904; leap < 2017; leap++) {
+			if(number  % 4 === 0 && number > 1903) {
 				return true	
 			}
 			else {
@@ -27,30 +20,63 @@ var leapYear = function(number) {
 	}
 
 console.log(leapYear)
-console.log(leapYear(1908))
+console.log(leapYear(1904))
 
-var nonArray = function(arr) {
-	for (var i = 1; i < arr.length; i++) {
-		if (arr[i] !== nonArray) {
-			return i
+var nonArray = function(number) {
+	for (var i = 0; i < arr.length; i++) {
+		if ( numbers.indexOf(i) !== number) {
+			return number
 		}
 	}
 }
 console.log(nonArray)
 console.log(nonArray[1, 2, 4, 5])
 
-var duplicate = function(arr) {
-	for (var i = 0; i < arr.length; i++) {
-		for (var j = 0; j = i; i++ )	{
-		if (arr[j] === i) {
-			return arr[i]
-		}
-		}
-	}
-}
-console.log(duplicate)
-console.log(duplicate[1, 1, 2, 4, 6, 4])
+// var duplicate = function(arr) {
+// 	for (var i = 0; i < arr.length; i++) {
+// 		for (var j = 0; j < i; j++ )	{
+// 		if (arr[j] === i) {
+// 			return arr[i]
+// 		}
+// 		}
+// 	}
+// }
+// console.log(duplicate)
+// console.log(duplicate[1, 1, 2, 4, 6, 4])
 
-var pigLatin = function(text) {
+// var pigLatin = function(text) {
+// 	return text.split('')
+// }
+// var translate = function(text) {
+// 	var chars = letters(text) 
+// 	return chars.slice[1].join('') + chars [0] + 'ay'
+// }		
+// var newWord = function(word) {
+// 	return word.replace(translate )
+// }
+
 	
+
+// console.log(pigLatin)
+// console.log(pigLatin('hello '))
+
+
+// sameValue = function(arr){
+
+// }
+
+// 8. 
+var arr = []
+while(arr.length < 10){
+	var randomNumber = Math.floor(Math.random()*101)
+	if(arr.indexOf(randomNumber) > -1) continue;
+	arr[arr.length] = randomNumber
 }
+	
+
+console.log(arr)
+
+
+
+
+
